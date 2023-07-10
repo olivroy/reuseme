6008ba4c5d14831165ed9e691d83883aea92fc6d
olivroy <olivierroy71@hotmail.com>
2023-07-10 12:44:58 -0400

    fix

--- R/open.R
+++ R/open.R
@@ -2,7 +2,8 @@
 #'
 #' Wrapper around [rstudioapi::documentOpen]
 #' @inheritParams rstudioapi::documentOpen
-#'
+#' @param move_cursor Boolean; move the cursor to the requested location after
+#'   opening the document?
 #' @return Invisibly returns the document id
 #' @export
 open_rs_doc <- function(path, line = -1L, col = -1L, move_cursor = TRUE) {
