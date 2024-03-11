58460060fb6293d1b297635d6aa136a89b3a1fc1
olivroy <olivierroy71@hotmail.com>
2024-03-11 11:51:33 -0400

    add proj_switch in same session

--- R/proj-list.R
+++ R/proj-list.R
@@ -2,19 +2,26 @@
 #'
 #' If not specified, will generate hyperlinks that call [usethis::proj_activate()].
 #' `proj_switch()` looks at `options(reuseme.reposdir)`.
+#'
 #' @param proj the name of a project located in the default locations or `NULL`
+#' @param new_session Should open in a new session?
 #'
 #' @returns Single logical value indicating if current session is modified.
 #' @export
 #' @seealso [usethis::proj_activate()]
-proj_switch <- function(proj = NULL) {
+proj_switch <- function(proj = NULL, new_session = TRUE) {
   # This is my default places (reuseme.reposdir possibly)
   # See fs::path_home
   all_projects <- proj_list()
 
   if (!is.null(proj)) {
     rlang::arg_match0(proj, values = names(all_projects))
-    usethis::proj_activate(unname(all_projects[proj]))
+    if (new_session) {
+      usethis::proj_activate(unname(all_projects[proj]))
+    } else {
+      # remove if https://github.com/r-lib/usethis/pull/1954 is implemented.
+      rstudioapi::openProject(path = unname(all_projects[proj]), newSession = FALSE)
+    }
     return(invisible(all_projects[proj]))
   }
 
