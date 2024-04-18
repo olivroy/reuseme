4ecd743b66df7be455c4870af30f6cf0a0adbf33
olivroy <olivierroy71@hotmail.com>
2024-04-17 21:10:07 -0400

    Move to own file

---
+++ R/files-conflicts.R
@@ -0,0 +1,177 @@
+#' Check if files referenced in source files exist in a current dir
+#'
+#' 1. It goes through the source files (.R/.qmd etc.),
+#' 2. It identifies data files (.csv, .xlsx) read or written
+#' 3. Search on the system if these files exist.
+#'
+#' Still WIP, so you can add code for false positive as needed.
+#'
+#' @param path a directory to search for
+#' @param quiet Whether it should print messages?
+#' @returns A logical
+#' @export
+#' @keywords internal
+#' @details
+#' To find genuine referenced files, we exclude different paths
+#'
+#' 1. Those created with `fs::path()` or `file.path()` or `glue::glue()`
+#' 2. Those that are checked for `fs::file_exists()`, `file.exists()`
+#' 3. Deleted with `fs::file_delete()`, `unlink()`
+check_referenced_files <- function(path = ".", quiet = FALSE) {
+  # TODO insert in either proj_outline, or rename_file
+  if (path == "." || fs::is_dir(path)) {
+    path <- fs::dir_ls(path = path, recurse = TRUE, regexp = "\\.(R|md|ml)$")
+    path <- fs::path_filter(path = path, regexp = "_files|tests/testthat", invert = TRUE) # need to do this in 2 places
+  } else if (fs::path_ext(path) %in% c("R", "yml", "yaml", "Rmd", "md", "qmd", "Rmarkdown")) {
+    path <- path
+  } else {
+    cli::cli_abort("Wrong specification.")
+  }
+
+  # TODO probably needs a `detect_genuine_path()`
+  referenced_files <- get_referenced_files(path)
+
+  files_detected <- unique(referenced_files)
+  references_a_non_existent_file <- !(fs::file_exists(files_detected) | file.exists(files_detected)) # to avoid burden for now.
+  if (!any(references_a_non_existent_file)) {
+    if (!quiet) cli::cli_inform(c("v" = "Did not find references to non-existent files."))
+    return(invisible())
+  }
+  # maybe order (so link to location) isn't quite right when many are found?
+  non_existent_files <- files_detected[references_a_non_existent_file]
+  if (quiet) {
+    cli::cli_warn(
+      c(
+        "Found {length(non_existent_files)} referenced file{?s} in folder.",
+        i = "See {.help reuseme::check_referenced_files} for more info.",
+        "There are locations in source files (qmd, Rmd, R) where a non-existent file (.csv, .xlsx etc.) is referenced.",
+        "Run {.code check_referenced_files(quiet = FALSE)} to see where this file is referenced.",
+        "{non_existent_files_show}"
+      ),
+      call = expr(check_referenced_files())
+    )
+  } else {
+    solve_file_name_conflict(
+      files = path,
+      regex = paste0(non_existent_files, collapse = "|"),
+      extra_msg = "Check in source files and rename the referenced (csv, xlsx etc.) files accordingly.",
+      quiet = quiet,
+      what = "to non-existent files"
+    )
+  }
+  invisible(non_existent_files)
+}
+
+#' Check if outdated or non-existent file is.
+#'
+#' If `quiet = FALSE` (default) will give a hint of where the file is referenced.
+#'
+#' @param files which files to search in
+#' @param regex a regex related to the file name to search for
+#' @param dir A directory where to operate
+#' @param extra_msg Extra message to pass
+#' @param what Which file conflicts we talking about
+#' @param quiet A logical, informs where the occurrences are found. (Default, `FALSE`)
+#'
+#' @return `FALSE` if no occurrences were found. `TRUE` if non-existent files
+#'   are referenced
+#' @export
+#' @keywords internal
+solve_file_name_conflict <- function(files, regex, dir = ".", extra_msg = NULL, quiet = FALSE, what = NULL) {
+  regex <- stringr::str_replace_all(regex, "\\\\|\\)|\\(|\\}\\{\\?|\\$|~", ".")
+  # regex <- as.character(regex)
+  if (dir != ".") {
+    cli::cli_abort("Don't know how to do this.")
+  }
+  bullets_df <- files |>
+    purrr::set_names() |>
+    purrr::map(\(x) readLines(x, encoding = "UTF-8")) |>
+    purrr::map(\(x) tibble::enframe(x, name = "line_number", value = "content")) |>
+    dplyr::bind_rows(.id = "file")
+
+  bullets_df <- bullets_df[grepl(regex, bullets_df$content), ]
+
+  if (nrow(bullets_df) == 0) {
+    return(FALSE)
+  }
+
+
+  if (!quiet) {
+    lines_match <- bullets_df$line_number
+    # Derive column match
+    start_end_pos <- stringr::str_locate(bullets_df$content, regex)
+    cols_match <- dplyr::coalesce(
+      start_end_pos[, "start"] - 1L,
+      0L
+    )
+    # Create hyperlinks from lines and columns
+    bullets <- stringr::str_glue(
+      "{{.file {bullets_df$file}:{lines_match}:{cols_match}}}"
+    )
+
+    if (length(bullets) > 20) {
+      # display_msg <- cli::format_inline("[10 first references only]")
+      # Showing First ten to avoid screen overflow.
+      # bullets_to_display <- cli::ansi_collapse(bullets[seq_len(10)])
+    } else {
+      # display_msg <- NULL
+
+      # bullets_to_display <- cli::ansi_collapse(bullets)
+    }
+
+    # Will truncate 20 (revert up if you don't like)
+    display_msg <- NULL
+    bullets_to_display <- cli::ansi_collapse(bullets)
+    # Remove duplicated Found x references
+    which_bullet_to_replace <- stringr::str_subset(extra_msg, "Found references to", negate = TRUE)
+    # possibly just move up our
+    # extra_msg[i] <-
+    cli::cli_bullets(c(
+      extra_msg,
+      "i" = paste0("Found {length(bullets)} reference{?s} ", what, " in ", bullets_to_display, "."),
+      display_msg
+    ))
+  } else {
+    cli::cli_inform(
+      c(
+        extra_msg,
+        "run {.run reuseme::check_referenced_files(quiet = TRUE)} to see where the conflicts are."
+      ),
+      .frequency = "always", .frequency_id = "nonexistantfiles"
+    )
+  }
+
+  invisible(TRUE)
+}
+
+# Helpers ----------------
+
+get_referenced_files <- function(files) {
+  # Create a list of genuine referenced files
+  # TODO Add false positive references
+  # TODO fs::path and file.path should be handled differently
+  path |>
+    purrr::map(\(x) readLines(x, encoding = "UTF-8")) |>
+    purrr::list_c(ptype = "character") |>
+    stringr::str_subset(pattern = "\\:\\:dav.+lt|\\:\\:nw_|g.docs_l.n|target-|\\.0pt", negate = TRUE) |> # remove false positive from .md files
+    stringr::str_subset(pattern = "file.path|fs\\:\\:path\\(", negate = TRUE) |> # Exclude fs::path() and file.path from search since handled differently.
+    stringr::str_subset(pattern = "file.[(exist)|(delete)]|glue\\:\\:glue|unlink", negate = TRUE) |> # don't detect where we test for existence of path or construct a path with glue
+    stringr::str_subset(pattern = "[(regexp)|(pattern)]\\s\\=.*\".*[:alpha:]\"", negate = TRUE) |> # remove regexp = a.pdf format
+    stringr::str_subset(pattern = "grepl?\\(|stringr", negate = TRUE) |> # avoid regexp
+    stringr::str_subset(pattern = "nocheck", negate = TRUE) |> # remove nocheck and unlink statements (refers to deleted files anywa)
+    stringr::str_subset("\"") |>
+    stringr::str_trim() |>
+    stringr::str_extract_all("\"[^\"]+\"") |>
+    unlist() |>
+    stringr::str_remove_all("\",?") |>
+    stringr::str_subset(pattern = "\\.\\w{1,6}$") |> # file pattern
+    stringr::str_subset(pattern = "\\.plot|\\.fr$|\\.frame|\\.obs$|\\.\\d{2,}$", negate = TRUE) |> # Manually add file exts that are not file exts.
+    stringr::str_subset(pattern = "tmp|temp", negate = TRUE) |> # remove common file names that are not very nice
+    stringr::str_subset(pattern = "https?", negate = TRUE) |> # doesn't check for files read online.
+    stringr::str_subset(pattern = "\\@.+\\.", negate = TRUE) |> # email addresses or containing @
+    stringr::str_subset(pattern = "_fichiers/", negate = TRUE) |> # manually remove false positive
+    stringr::str_subset(pattern = "\n", negate = TRUE) |> # remove things with line breaks
+    stringr::str_subset(pattern = "^\\.[:alpha:]{1,4}$", negate = TRUE) |> # remove reference to only file extensions
+    stringr::str_subset(pattern = "\\.\\d+$", negate = TRUE) |> # remove 0.000 type
+    purrr::set_names()
+}
