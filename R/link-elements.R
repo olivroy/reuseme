a6444cd28a3faba396ac287cb53bee29d584f495
olivroy <olivierroy71@hotmail.com>
2024-04-21 16:15:41 -0400

    Add `link_issue()`

---
+++ R/link-elements.R
@@ -0,0 +1,35 @@
+#' Create a cli link to a GitHub issue
+#'
+#' In RStudio, links to issues are automatically recognized.
+#' This function creates cli ansi links to entries of the form rstudio/rstudio#1100
+#'
+#' Note: doesn't support without <OWNER>/<REPO>
+#' @param x A string, usually lines of files that contains issue numbers.
+#'
+#' @return A formatted linked issue to GitHub issue
+#' @export
+#'
+#' @examples
+#' link_issue(c("We really need rstudio/gt#1469 to be fixed.")) |> cli::cli_bullets()
+link_issue <- function(x) {
+  # Return early if no issue pattern is detected.
+  if (!any(stringr::str_detect(x, "[:graph:]+/[^#\\s]+#\\d"))) {
+    return(x)
+  }
+
+  li <- stringr::str_split(x, pattern = "\\s")
+
+  issue_regex <- "([:graph:]+/[^#\\s]+)#(\\d+)"
+
+  res <- purrr::map(li, function(x) {
+    purrr::map_chr(x, function(y) {
+      if (stringr::str_detect(y, issue_regex)) {
+        rep <- stringr::str_replace_all(y, "([:graph:]+/[^#\\s]+)#(\\d+)", "https://github.com/\\1/issues/\\2")
+
+        y <- paste0("{.href [", y, "](", rep, ")}")
+      }
+      y
+    })
+  })
+  purrr::map_chr(res, \(x) paste(x, collapse = " "))
+}
---
+++ tests/testthat/_snaps/link-elements.md
@@ -0,0 +1,8 @@
+# link_issue() works
+
+    Code
+      lines <- c("Go to rstudio/rstudio#120")
+      cat(link_issue(lines))
+    Output
+      Go to {.href [rstudio/rstudio#120](https://github.com/rstudio/rstudio/issues/120)}
+
