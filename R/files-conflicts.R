5d7fcf5d30fe1441eeb089f2d31f8e7647289868
olivroy <olivierroy71@hotmail.com>
2024-06-26 08:43:44 -0400

    Look in .Rbuildignore when renaming files. Will still need to escape things to work properly

--- R/files-conflicts.R
+++ R/files-conflicts.R
@@ -20,9 +20,10 @@
 check_referenced_files <- function(path = ".", quiet = FALSE) {
   # TODO insert in either proj_outline, or rename_file
   if (path == "." || fs::is_dir(path)) {
-    path <- fs::dir_ls(path = path, recurse = TRUE, regexp = "\\.(R|md|ml)$")
-    path <- fs::path_filter(path = path, regexp = "_files|tests/testthat|_book|_freeze", invert = TRUE) # need to do this in 2 places
-  } else if (fs::path_ext(path) %in% c("R", "yml", "yaml", "Rmd", "md", "qmd", "Rmarkdown")) {
+    # FIXME in Rbuilignore, change `^_pkgdown\.yml$` to `_pkgdown.yml` to make sure it works
+    path <- fs::dir_ls(path = path, recurse = TRUE, regexp = "\\.(R|md|ml|builignore)$", all = TRUE, type = "file")
+    path <- fs::path_filter(path = path, regexp = "_files|tests/testthat|_book|_freeze|.github", invert = TRUE) # need to do this in 2 places
+  } else if (fs::path_ext(path) %in% c("R", "yml", "yaml", "Rmd", "md", "qmd", "Rmarkdown", "gitignore", "Rbuildignore")) {
     path <- path
   } else {
     cli::cli_abort("Wrong specification.")
