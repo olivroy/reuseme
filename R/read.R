346c55a4d7f975a329770066a9e6c5186f644153
olivroy <olivierroy71@hotmail.com>
2025-01-02 13:10:56 -0500

    Add a sketch of `read_clean()`

--- R/read.R
+++ R/read.R
@@ -1,6 +1,9 @@
 #' Read and clean names and set variable labels
 #'
-#' Reads data from file, but clean names with [janitor::make_clean_names()]
+#' Reads data from file, but clean names with [janitor::make_clean_names()].
+#'
+#' It keeps the variable labels attributes to have improved support in the RStudio IDE,
+#' with gt tables and with ggplots (>= 3.6.0).
 #'
 #'
 #' @inheritParams readr::read_csv
@@ -10,6 +13,7 @@
 #'
 read_clean <- function(file, ...) {
   file_ext <- fs::path_ext(file)
+  rlang::check_installed(c("labelled", "readr", "readxl", "janitor"))
   f <- switch(
     file_ext,
     "csv" = readr::read_csv,
@@ -19,7 +23,15 @@ read_clean <- function(file, ...) {
   )
 
   dat <- f(file, ...)
+  labels <- names(dat)
 
+  dat <- janitor::clean_names(dat)
+
+  if (!identical(names(dat), labels)) {
+    dat <- labelled::set_variable_labels(dat, .labels = labels)
+  }
+
+  dat
 
 
 }
